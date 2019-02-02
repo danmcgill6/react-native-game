a2266630d73ca9e52b40a7c7e22d535d800260f7
Dan Mcgillicuddy <danmcgillicuddy@Dans-MacBook-Pro.local>
2019-02-02 18:14:55 -0500

    merged with expo

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Text} from 'react-native';
 import Game from './components/game.js';
 
 export default class App extends React.Component {
--- components/game.js
+++ components/game.js
@@ -1,72 +1,25 @@
-import React from 'react';
+
 import Expo from 'expo';
 import ExpoPhaser from 'expo-phaser';
+import React from 'react';
 
-const Assets = {
-  'man.png': require('../assets/sprite.png'),
-  'man.json': require('../assets/sprite.json'),
-};
+import Playable from './states/Playable';
 
-export default class Game extends React.Component {
-  state = { loading: true };
-  async componentWillMount() {
-    const downloads = [];
-    for (let key of Object.keys(Assets)) {
-      const asset = Expo.Asset.fromModule(Assets[key]);
-      downloads.push(asset.downloadAsync());
-    }
-    await Promise.all(downloads);
-    this.setState({ loading: false });
+export default class Game {
+  constructor({ context }) {
+    Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.PORTRAIT);
+
+    const game = ExpoPhaser.game({ context });
+    this.playable = new Playable({ game, context });
+    game.state.add('Playable', this.playable);
+    game.state.start('Playable');
   }
-  render() {
-    if (this.state.loading) {
-      return <Expo.AppLoading />;
+
+  updateControls = velocity => {
+    if (this.playable) {
+      this.playable.updateControls({ velocity });
     }
-
-    return (
-      <Expo.GLView
-        style={{ flex: 1 }}
-        onContextCreate={context => startGame({ context })}
-      />
-    );
-  }
-}
-
-function startGame({ context }) {
-  const game = ExpoPhaser.game({ context });
-
-  game.state.add('Playable', {
-    preload: function() {
-      const atlas = Expo.Asset.fromModule(Assets['man.json']).localUri;
-      const texture = Expo.Asset.fromModule(Assets['man.png']).localUri;
-      game.load.atlasJSONHash('man', texture, atlas);
-    },
-    create: function() {
-      game.stage.backgroundColor = '#4488AA';
-
-      game.physics.startSystem(Phaser.Physics.ARCADE);
-
-      //  Set the world (global) gravity
-      game.physics.arcade.gravity.y = 100;
-
-      const man = game.add.sprite(200, 200, 'man');
-      game.physics.enable([man], Phaser.Physics.ARCADE);
-
-      //  Here we add a new animation called 'run'
-      //  We haven't specified any frames because it's using every frame in the texture atlas
-
-      man.animations.add('run');
-      man.body.collideWorldBounds = true;
-      man.body.bounce.y = 0.8;
-      man.body.gravity.y = 200;
-
-      //  And this starts the animation playing by using its key ("run")
-      //  15 is the frame rate (15fps)
-      //  true means it will loop when it finishes
-      man.animations.play('run', 15, true);
-    },
-    update: function() {},
-  });
-
-  game.state.start('Playable');
+  };
+  onTouchesBegan = () => this.playable && this.playable.onTouchesBegan();
+  onTouchesEnded = () => this.playable && this.playable.onTouchesEnded();
 }
